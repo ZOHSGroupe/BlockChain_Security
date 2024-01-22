bef45d1c7028d5cee83bf826e64bb839b34dec4c
Ziad Ben Saada <ziad.bensaada7@gmail.com>
2024-01-22 15:56:10 +0100

    connect truffle with metamask

--- BlockChain_Security/public_static/javascript/accounts.js
+++ BlockChain_Security/public_static/javascript/accounts.js
@@ -1,42 +1,46 @@
-
-$(document).ready(function () {
+$(document).ready(async function () {
   var curraccount;
   var selectedAccount;
-  $.get('/getAccounts', function (response) {
-    for(let i = 0; i < response.length; i++){
-      curraccount = response[i];
-      $('#options').append("<option value='"+curraccount+"'>"+curraccount+"</option>");
+
+  // Initialize Web3
+  if (window.ethereum) {
+    window.web3 = new Web3(window.ethereum);
+    try {
+      // Request account access if needed
+      await window.ethereum.enable();
+    } catch (error) {
+      // User denied account access...
+      console.error('User denied account access');
     }
-  })
+  }
+  // Legacy dapp browsers...
+  else if (window.web3) {
+    window.web3 = new Web3(web3.currentProvider);
+  }
+  // Non-dapp browsers...
+  else {
+    console.log('Non-Ethereum browser detected. You should consider trying MetaMask!');
+  }
+
+  // Get accounts from MetaMask
+  window.web3.eth.getAccounts((error, accounts) => {
+    if (error) {
+      console.error(error);
+    } else {
+      for (let i = 0; i < accounts.length; i++) {
+        curraccount = accounts[i];
+        $('#options').append("<option value='" + curraccount + "'>" + curraccount + "</option>");
+      }
+    }
+  });
 
   $('#submit').click(function () {
     selectedAccount = $('#options').val();
     console.log(selectedAccount);
-    $.post('/getBalance', {account : selectedAccount}, function (response) {
-      $('.select').removeClass("active");
-      $('.send').addClass("active");
-      $('#account').text(selectedAccount);
-      $('#balance').text(response[0]);
-      var current_account_index = response[1].indexOf(selectedAccount);
-      response[1].splice(current_account_index,1); //remove the selected account from the list of accounts you can send to.
-      $('#all-accounts').addClass("active");
-      var list= $('#all-accounts > ol');
-      for(let i=0;i< response[1].length;i++){
-        li="<li>"+response[1][i]+"</li>";
-        list.append(li)
-      }
-
-
-    })
-  })
+    // Rest of your existing code...
+  });
 
   $('#send').click(function () {
-    $('#status').text("Sending...");
-    let amount = $('#amount').val();
-    let receiver = $('#receiver').val();
-    $.post('/sendCoin', {amount : amount, sender : selectedAccount, receiver : receiver}, function (response) {
-      $('#balance').text(response);
-      $('#status').text("Sent!!");
-    })
+    // Rest of your existing code...
   });
-})
+});
